36d59546d378f5207cbc4c7bbb983ebb7f93139b
UCscottg8 <134948683+UCscottg8@users.noreply.github.com>
2023-05-31 21:54:58 -0400

    version 1.0 with values

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+        // Maintenance costs for each season
+        double springCost = 100;
+        double summerCost = 250;
+        double autumnCost = 300;
+        double winterCost = 350;
+
+        // Total yearly maintenance costs
+        double totalYearlyCost = springCost + summerCost + autumnCost + winterCost;
+
+        // Output statement
+        System.out.println("Spring maintenance cost: $" + springCost);
+        System.out.println("Summer maintenance cost: $" + summerCost);
+        System.out.println("Autumn maintenance cost: $" + autumnCost);
+        System.out.println("Winter maintenance cost: $" + winterCost);
+        System.out.println("Total yearly maintenance cost: $" + totalYearlyCost);
+    }
+}
